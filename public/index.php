9931c0b4368168c17679812c00bb2882af8dffb7
Scott Cordle <scordle@sullivan.edu>
2019-08-22 14:20:48 -0400

    added includes

--- public/index.php
+++ public/index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf=8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <meta name="viewport" content="width_divice-width, initials-cale=1" />
-
-    <title>Simple Database App</title>
-
-    <link rel="stylesheet" href="css/style.css" />
-</head>
-
-<body>
-    <h1>Simple Database App</h1>
+<?php include "templates/header.php"; ?>
 
     <ul>
     <li>
@@ -22,6 +8,5 @@
         <a href="read.php"><strong>Read</strong></a> - find a user
     </li>
 </ul>
-</body>
 
-</html>
+<?php include "templates/footer.php"; ?>
