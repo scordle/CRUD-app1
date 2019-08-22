83be2ed3dc3a31f6209e2a7bf3328629f9ff220f
Scott Cordle <scordle@sullivan.edu>
2019-08-22 16:29:16 -0400

    Rearranging file to correct directories

--- public/create.php
+++ public/create.php
@@ -1,24 +1,36 @@
-<?php include "templates/header.php"; ?>
+<?php require "templates/header.php"; ?>
 
-<form method="post">
+  <?php 
+    if (isset($_POST['submit']) {
+        require "../config.php";
+
+        try {
+            $connection = new PDO($dsn, $username, $password, $options);
+            // insert new user code will go here
+
+        } catch(PDOException $error) {
+            echo $sql . "<br>" . $error->getMessage();
+        }
+    }
+    
+    ?>
+
+  <h2>Add a user</h2>
+
+  <form method="post">
     <label for="firstname">First Name</label>
-    <input type="text" name="firstname" id="firstname" />
-
+    <input type="text" name="firstname" id="firstname">
     <label for="lastname">Last Name</label>
-    <input type="text" name="lastname" id="lastname" />
-
+    <input type="text" name="lastname" id="lastname">
     <label for="email">Email Address</label>
-    <input type="text" name="email" id="email" />
-
+    <input type="text" name="email" id="email">
     <label for="age">Age</label>
-    <input type="text" name="age" id="age" />
-
+    <input type="text" name="age" id="age">
     <label for="location">Location</label>
-    <input type="text" name="location" id="location" />
+    <input type="text" name="location" id="location">
+    <input type="submit" name="submit" value="Submit">
+  </form>
 
-    <input type="submit" name="submit" value="Submit" />
-</form>
+  <a href="index.php">Back to home</a>
 
-<a href="index.php">Back to home</a>
-
-<?php include "templates/footer.php"; ?>
+<?php require "templates/footer.php"; ?>
