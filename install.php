2d92c90534b4427807322485b04f8e75e8e9e72d
Scott Cordle <scordle@sullivan.edu>
2019-08-22 16:42:11 -0400

    attempting to debug data connection

--- install.php
+++ install.php
@@ -3,9 +3,10 @@
 /**
  * Open a connection via PDO to create a
  * new database and table with structure.
- *
  */
+
 require "config.php";
+
 try {
     $connection = new PDO("mysql:host=$host", $username, $password, $options);
     $sql = file_get_contents("data/init.sql");
